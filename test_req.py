b67bccbedff8bba39a121bb3379fb0864049776a
Aiden Hopkins <acdphc@qq.com>
2025-06-12 23:05:06 +0800

    ts

--- test_req.py
+++ test_req.py
@@ -3,7 +3,7 @@ import dlso
 ep = dlso.Endpoint(
     model='text-embedding-3-small',
     key='sk-fxZcN6GZbKn5hk6BjgwypFevWl5oO2rF6xNMA3YwVmZOR3WN',
-    endpoint='http://yunwu.ai/v1'
+    endpoint='https://yunwu.ai/v1'
 )
 
-print(ep.embed('hello world'))
+print(ep.embed('hello world', 'text-embedding-3-large'))
